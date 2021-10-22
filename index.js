fb42df5943405c06b2b1c3991a43eff6d7242085
Alexander Melo <alexandermelox@gmail.com>
2021-10-22 12:13:13 -0400

    able to add a song

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-const ytdl = require('ytdl-core');
+const { execute } = require('./functions');
+const queue = require('./queue');
 
 /**
  * Your .env file should include the prefix and token of your bot
@@ -20,4 +21,27 @@ client.once('reconnecting', () => {
 
 client.once('disconnect', () => {
   console.log('Disconnect!');
+});
+
+client.on('message', async (message) => {
+  const { author, content, guild } = message;
+  const serverQueue = queue.get(guild.id);
+
+  // checks if the message is from the bot
+  if (author.bot) return;
+  // checks if the message starts with the prefix
+  if (!content.startsWith(PREFIX)) return;
+
+  if (content.startsWith(`${PREFIX}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (content.startsWith(`${PREFIX}skip`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (content.startsWith(`${PREFIX}stop`)) {
+    execute(message, serverQueue);
+    return;
+  } else {
+    message.channel.send('You need to enter a valid command!');
+  }
 });
